0bf108a49aac50c2f539c154fcc8c67b5e4a00e0
abusasa <abdullaabusamat2@gmail.com>
2026-03-08 22:52:32 +0500

    update music and index

--- zhaina/jscp/settings.js
+++ zhaina/jscp/settings.js
@@ -368,7 +368,7 @@ function initializeDefaultSettings() {
             // --- Lembar 2 (data-page="1") ---
             { 
                 image: './image/photos/b2.png', // Menggantikan fjr3qh.png (Index 2)
-                content: 'Happy Birthday Sayang 💗' 
+                content: 'Happy Womens Day Жайна 💗' 
             },
             { 
                 image: './image/photos/b3.png', // Menggantikan 8u5tf7.png (Index 3)
